3f4f6bcaea1c454150b4b14f42b8cff2ee6428d2
Toeiiz <32189965+s5935512064@users.noreply.github.com>
2022-04-26 01:35:09 +0700

    xx

--- components/NewsCarousel.js
+++ components/NewsCarousel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import axios from "axios";
 import useSWR, { mutate } from "swr";
 import moment from "moment";
@@ -12,11 +12,26 @@ moment.locale("th");
 
 const URL2 = "https://polomanage.ssdapp.net/booking_status.php?date=2022-04-22";
 
-const URL = `https://graph.facebook.com/v13.0/PoloFootballPark/feed?fields=full_picture,permalink_url,message,created_time&limit=15&access_token=EAAooxZBZBd3LcBAPdOHKvdEET61eDH1ah8mBVnXLJhaoIYBwGUKUimZBi7eZBZBbvyjjLa5tzprhTTU8ZCiRkEYY1D2qtY5mL6sVWpKrZAUVrNZAxRfXAZC5S1u6miEzMgzfcWYjmehEjQxD66m9BE1ar4OOnebhFMzAAS4OECCjQko5vZAIxkCFxJ6ZBjOOu18htJWjBb4VZAKYXgZDZD&format=json`;
+// const URL = `https://graph.facebook.com/v13.0/PoloFootballPark/feed?fields=full_picture,permalink_url,message,created_time&limit=15&access_token=EAAooxZBZBd3LcBAPdOHKvdEET61eDH1ah8mBVnXLJhaoIYBwGUKUimZBi7eZBZBbvyjjLa5tzprhTTU8ZCiRkEYY1D2qtY5mL6sVWpKrZAUVrNZAxRfXAZC5S1u6miEzMgzfcWYjmehEjQxD66m9BE1ar4OOnebhFMzAAS4OECCjQko5vZAIxkCFxJ6ZBjOOu18htJWjBb4VZAKYXgZDZD&format=json`;
 
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+// const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const NewsCarousel = () => {
+  // const wrapper = useRef(null);
+  // useEffect(() => {
+  //   const items = props.items || [];
+  //   const opts = props.options || {};
+
+  //   opts.slides = [...items].map((val) => {
+  //     return { html: val };
+  //   });
+
+  //   const instance = new NativeCarousel(wrapper.current, opts);
+
+  //   return () => {
+  //     instance.destroy();
+  //   };
+  // }, []);
   //   const [news, setNews] = useState([]);
   //   const getNews = async (e) => {
   //     const res = await axios.get(URL).then((response) => {
@@ -47,32 +62,40 @@ const NewsCarousel = () => {
   //     }
   //   };
 
-  const { data, error } = useSWR(URL, fetcher);
-  if (!data) {
-    return <div> Loading ..</div>;
-  }
+  // const { data, error } = useSWR(URL, fetcher);
+  // if (!data) {
+  //   return <div> Loading ..</div>;
+  // }
 
-  useEffect(() => {
-    const MainCarousel = new Carousel(document.querySelector("#MainCarousel"), {
-      ScrollLock: false,
-      Autoplay: {
-        timeout: 1000,
-        hoverPause: true,
-      },
-    });
-  }, []);
+  // useEffect(() => {
+  //   const MainCarousel = new Carousel(document.querySelector("#MainCarousel"), {
+  //     ScrollLock: false,
+  //     Autoplay: {
+  //       timeout: 1000,
+  //       hoverPause: true,
+  //     },
+  //   });
+  // }, []);
 
   return (
     <>
-      <div id="MainCarousel" className="carousel mb-4 w-full mx-auto">
+      {/* <div
+        className={`carousel ${props.class || ""}`}
+        ref={wrapper}
+        items={["1", "2", "3", "4", "5"]}
+        options={{ infinite: false }}
+      ></div> */}
+      {/* <div id="cardSlider" className="carousel mb-4 w-full mx-auto">
         <div className="carousel__viewport px-12">
-          {news.map((item, index) => (
-            <figure key={index} className="carousel__slide py-0 px-4 w-1/3">
-              <img className="mb-4 w-full rounded-lg" src={item.full_picture} />
-            </figure>
-          ))}
+          <figure className="carousel__slide py-0 px-4 w-1/3">
+            <img
+              className="mb-4 w-full rounded-lg"
+              src="https://polofootballpark.com/images/post/e7d053b893d547383895457cf582579f-1463371734.jpg"
+              alt="profile"
+            />
+          </figure>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
