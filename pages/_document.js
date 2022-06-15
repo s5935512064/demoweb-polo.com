34ae9b7134808e4137736699587088369d0a1fdb
s5935512064 <32189965+s5935512064@users.noreply.github.com>
2022-06-15 17:51:23 +0700

    fixed bug script

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
-import React, { Fragment, useState, useEffect } from 'react'
-
 
 export default function Document() {
 
@@ -12,8 +9,6 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-
-                <Script defer src="../components/cookieconsent.js" />
 
             </body>
         </Html>
